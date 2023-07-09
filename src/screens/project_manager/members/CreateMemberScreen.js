5de996e7fa8c45dd3404d129d7c04d5dec7ba21b
Dilpreet Brar <dilpreet.brar.professional@gmail.com>
2023-07-09 02:32:58 -0400

    add add new member functions

--- src/screens/project_manager/members/CreateMemberScreen.js
+++ src/screens/project_manager/members/CreateMemberScreen.js
@@ -1,41 +1,86 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import theme from '../../../theme/theme';
-import commonStyles from '../../../theme/commonStyles';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { getUserData } from "../../../store/creds";
+import { searchUsers, createUser } from "../../../store/user"
 
 const CreateMemberPage = () => {
   const navigation = useNavigation();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [hidePassword, setHidePassword] = useState(true);
   const [hourlyRate, setHourlyRate] = useState('');
 
   const handleCancel = () => {
     navigation.goBack();
   };
 
-  const handleSave = () => {
-    // Handle save action
+  const handleSave = async () => {
+    if (!email || !password || !hourlyRate) {
+      alert('Please fill in all fields.');
+      return;
+    }
+  
+    if (!isValidEmail(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+  
+    if (password.length < 8) {
+      alert('Password should be at least 8 characters long.');
+      return;
+    }
+  
+    if (!isValidHourlyRate(hourlyRate)) {
+      alert('Please enter a valid hourly rate.');
+      return;
+    }
+
+    let userExists = await searchUsers(email);
+
+    if (userExists.length > 0) {
+      alert("User already exists");
+      return;
+    }
+
+    user = await getUserData();
+
+    // If validation passes and user doesn't exist, create user
+    await createUser(email, password, "admin", 0.0, user.email); // Update the type and hourly_rate as per your requirements
+
+    // Then redirect the user to the login screen or anywhere you want
+    handleCancel();
   };
 
-  const toggleHidePassword = () => {
-    setHidePassword(!hidePassword);
+  const isValidEmail = (email) => {
+    // Email validation logic
+    // You can use regular expressions or any other validation library to validate the email format
+    // Here's a simple example using a regular expression
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+  };
+  
+  const isValidHourlyRate = (hourlyRate) => {
+    // Hourly rate validation logic
+    // You can check if the hourly rate is a valid float greater than or equal to 0
+    const rate = parseFloat(hourlyRate);
+    return !isNaN(rate) && rate >= 0;
   };
 
   return (
     <View style={styles.container}>
       <View style={{
-          flexDirection: "row",  
-          marginTop: 30  
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',  
+          marginTop: 30,
         }}>
         <TouchableOpacity onPress={handleCancel}>
           <Text>Cancel</Text>
         </TouchableOpacity>
         <Text>Create Member</Text>
         <TouchableOpacity onPress={handleSave}>
-          <Text>Save Member</Text>
+          <Text>Save</Text>
         </TouchableOpacity>
       </View>
       <TextInput
@@ -44,24 +89,12 @@ const CreateMemberPage = () => {
         value={email}
         placeholder="Email"
       />
-      <View style={commonStyles.inputContainer}>
-        <Text style={commonStyles.inputLabel}>Password</Text>
-        <View style={styles.passwordContainer}>
-          <TextInput
-            style={styles.passwordInput}
-            autoCapitalize='none'
-            onChangeText={setPassword}
-            value={password}            
-            secureTextEntry={hidePassword}
-          />
-          <TouchableOpacity style={styles.iconContainer} onPress={toggleHidePassword}>
-            <Image 
-              source={hidePassword ? require('../../../../assets/eye-closed.png') : require('../../../../assets/eye-open.png')} 
-              style={styles.icon} 
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Password"
+      />
       <TextInput
         style={styles.input}
         onChangeText={setHourlyRate}
@@ -90,28 +123,6 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 10,
     marginBottom: 10,
-  },
-  passwordContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: theme.colors.grey,
-    borderRadius: 5,
-    width: '100%',
-    alignSelf: 'center',
-  },
-  passwordInput: {
-    paddingHorizontal: 12,    
-    paddingVertical: 10,
-    minWidth: "88%"
-  },
-  iconContainer: {
-    padding: 10,
-  },
-  icon: {
-    width: 20,   // Or any other dimensions you want
-    height: 16,
   },
 });
 
