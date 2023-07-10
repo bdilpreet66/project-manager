810bfd092ff0055230d1f5f428d3fea1a8ad3b51
LCBO\stantoy <sbabaran@gmail.com>
2023-07-09 23:01:14 -0400

    added sroll view for project view

--- src/screens/project_manager/project/ViewProjectScreen.js
+++ src/screens/project_manager/project/ViewProjectScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, FlatList } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, FlatList, Dimensions, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import commonStyles from '../../../theme/commonStyles';
 import { updateProjectByID, getTasksByProject } from './../../../store/project'
@@ -10,6 +10,8 @@ const ViewProjectScreen = () => {
   const { project } = route.params;
   const navigation = useNavigation();
   const [tasks, setTasks] = useState([]);
+
+  const screenWidth = Dimensions.get('window').width - 40;
 
   const [projectData, setProjectData] = useState({
     id: project.id,
@@ -38,76 +40,95 @@ const ViewProjectScreen = () => {
     }
   };
 
+  const renderItem = ({ item }) => (    
+    <TouchableOpacity style={[styles.listItem]} onPress={() => navigation.navigate('View Task', { project: project, task: item })}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Text>{item.id}# {item.name}</Text>
+        <Text>Due Date: {item.end_date}</Text>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          {item.status == "pending" ? 
+          <Text style={[commonStyles.badge,commonStyles.badgeWarning,styles.badge]}>{item.status}</Text> : 
+          <Text style={[commonStyles.badge,commonStyles.badgeDefault,styles.badge]}>{item.status}</Text>
+          }
+          <Text>Assigned To: {item.assigned_to}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <>
-    <View style={styles.ctaContainer}> 
-      <TouchableOpacity onPress={() => navigation.navigate('Project List')}>
-        <Text style={commonStyles.labelTopNav}>Cancel</Text>
-      </TouchableOpacity>      
-      <Text style={[commonStyles.labelTopNavHeading,commonStyles.bold]}>Project Details</Text>
-      <TouchableOpacity onPress={handleSave}>
-        <Text style={commonStyles.labelTopNav}>Save</Text>
-      </TouchableOpacity>      
-    </View>  
-    <View style={[commonStyles.container, styles.container]}>      
-      <View style={styles.inputContainer}>
-        <Text style={commonStyles.inputLabel}>Name</Text>
-        <TextInput
-            value={projectData.name}                
-            style={commonStyles.input}
-            onChangeText={(text) => setProjectData({ ...projectData, name: text })}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={commonStyles.inputLabel}>Description</Text>
-        <TextInput
-            value={projectData.description}            
-            multiline
-            numberOfLines={4}
-            style={[commonStyles.input,{ height: 140 }]}
-            onChangeText={(text) => setProjectData({ ...projectData, description: text })}
-        />                                    
-      </View>
-      <View style={[styles.inputContainer]}>
-        <Text style={commonStyles.inputLabel}>Status</Text>
-      </View>
-      <View style={[styles.staticContent]}>
-        {projectData.status === 'pending' ?
-          <Text style={[commonStyles.inputLabel,commonStyles.badge,commonStyles.badgeWarning,styles.status]}>Incomplete</Text>
-        :
-          <Text style={[commonStyles.inputLabel,]}>Completed</Text>
-        }        
-      </View>            
-      <View style={styles.inputContainer}>
-        <Text style={commonStyles.inputLabel}>Total Cost</Text>        
-      </View>            
-      <View style={[styles.staticContent]}>
-        <Text style={[commonStyles.inputLabel,styles.staticContent]}>$100.00</Text>
-        <Text style={[commonStyles.link,commonStyles.underline]}>View Logs</Text>
-      </View>
-    </View>       
-    <View>      
-      <FlatList
-        data={tasks}
-        keyExtractor={item => item.id.toString()} // Assuming 'id' is a unique identifier
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.name}</Text>
-            <Text>{item.description}</Text>
-            <Text>Start Date: {item.start_date}</Text>
-            <Text>End Date: {item.end_date}</Text>
-            <Text>Assigned to: {item.assigned_to}</Text>
-            <Text>Status: {item.status}</Text>
-          </View>
+    <ScrollView>
+      <View style={styles.ctaContainer}> 
+        <TouchableOpacity onPress={() => navigation.navigate('Project List')}>
+          <Text style={commonStyles.labelTopNav}>Cancel</Text>
+        </TouchableOpacity>      
+        <Text style={[commonStyles.labelTopNavHeading,commonStyles.bold]}>Project Details</Text>
+        <TouchableOpacity onPress={handleSave}>
+          <Text style={commonStyles.labelTopNav}>Save</Text>
+        </TouchableOpacity>      
+      </View>  
+      <View style={[commonStyles.container, styles.container]}>      
+        <View style={styles.inputContainer}>
+          <Text style={commonStyles.inputLabel}>Name</Text>
+          <TextInput
+              value={projectData.name}                
+              style={commonStyles.input}
+              onChangeText={(text) => setProjectData({ ...projectData, name: text })}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={commonStyles.inputLabel}>Description</Text>
+          <TextInput
+              value={projectData.description}            
+              multiline
+              numberOfLines={4}
+              style={[commonStyles.input,{ height: 140 }]}
+              onChangeText={(text) => setProjectData({ ...projectData, description: text })}
+          />                                    
+        </View>
+        <View style={[styles.inputContainer]}>
+          <Text style={commonStyles.inputLabel}>Status</Text>
+        </View>
+        <View style={[styles.staticContent]}>
+          {projectData.status === 'pending' ?
+            <Text style={[commonStyles.inputLabel,commonStyles.badge,commonStyles.badgeWarning,styles.status]}>Incomplete</Text>
+          :
+            <Text style={[commonStyles.inputLabel,]}>Completed</Text>
+          }        
+        </View>            
+        <View style={styles.inputContainer}>
+          <Text style={commonStyles.inputLabel}>Total Cost</Text>        
+        </View>            
+        <View style={[styles.staticContent]}>
+          <Text style={[commonStyles.inputLabel,styles.staticContent]}>$100.00</Text>
+          <Text style={[commonStyles.link,commonStyles.underline]}>View Logs</Text>
+        </View>
+      </View>       
+      <View>
+        {tasks.map(
+          (item) => (    
+            <TouchableOpacity style={[styles.listItem]} onPress={() => navigation.navigate('View Task', { project: project, task: item })}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Text>{item.id}# {item.name}</Text>
+                <Text>Due Date: {item.end_date}</Text>
+              </View>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                  {item.status == "pending" ? 
+                  <Text style={[commonStyles.badge,commonStyles.badgeWarning,styles.badge]}>{item.status}</Text> : 
+                  <Text style={[commonStyles.badge,commonStyles.badgeDefault,styles.badge]}>{item.status}</Text>
+                  }
+                  <Text>Assigned To: {item.assigned_to}</Text>
+              </View>
+            </TouchableOpacity>
+          )
         )}
-      />
-    </View> 
-    <View style={styles.ctaContainer}>      
-      <TouchableOpacity style={[commonStyles.button,commonStyles.buttonPrimary,styles.button]} onPress={() => navigation.navigate('Create Task',{ project: projectData } )}>
-        <Text style={[commonStyles.buttonText,commonStyles.buttonTextPrimary]}>Create Task</Text>
-      </TouchableOpacity>  
-    </View>   
-    </>
+      </View> 
+      <View style={styles.ctaContainer}>      
+        <TouchableOpacity style={[commonStyles.button,commonStyles.buttonPrimary,styles.button]} onPress={() => navigation.navigate('Create Task',{ project: projectData } )}>
+          <Text style={[commonStyles.buttonText,commonStyles.buttonTextPrimary]}>Create Task</Text>
+        </TouchableOpacity>  
+      </View>   
+    </ScrollView>
   );
 };
 
